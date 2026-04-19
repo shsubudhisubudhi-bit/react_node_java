90d449fca60a3c92ab8a50a4455196151596774b
root <root@JavaServer.bjva5oiovhwuzary1w2uy2x2of.rx.internal.cloudapp.net>
2026-04-19 10:52:37 +0000

    'public ip ref

--- node-backend/config/index.js
+++ node-backend/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  GO_BACKEND_URL: process.env.GO_BACKEND_URL || 'http://localhost:8080',
+  GO_BACKEND_URL: process.env.GO_BACKEND_URL || 'http://98.70.44.45:8080',
   NODE_ENV: process.env.NODE_ENV || 'development'
 };
